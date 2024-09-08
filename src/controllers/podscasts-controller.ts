0a784af1f161c2bc510e2a4591e040da5f6c72f7
Victor Moreira <zvihugo@hotmail.com>
2024-09-07 22:49:17 -0300

    refactoring controllers/services

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -15,8 +15,8 @@ export const getFilerEpisodes = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
-  const queryString = req.url?.split("?p=")[1] || "";
-  const content = await serviceFilterEpisodes(queryString);
+  
+  const content = await serviceFilterEpisodes(req.url);
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(JSON.stringify(content));
 };
--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,8 +1,12 @@
+import { IncomingMessage } from "http";
 import { repositoryPodcast } from "../repositories/podscats-repository"
 
 
-export const serviceFilterEpisodes = async (podcastName: string) => {
-    const data = await repositoryPodcast(podcastName);
+export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
+
+    const queryString = podcastName?.split("?p=")[1] || "";
+
+    const data = await repositoryPodcast(queryString);
 
     return data;
 }
